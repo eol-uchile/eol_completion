bc6ab1f783e54aa85ebfbe48a796a4b96c2484fd
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2026-05-26 17:20:53 -0400

    feat: add api with get_completion_per_user

--- eol_completion/api.py
+++ eol_completion/api.py
@@ -1,2 +1,24 @@
-def get_completion():
-    return True
+# Installed packages (via pip)
+from django.contrib.auth.models import User
+
+# Edx dependencies
+from common.djangoapps.student.models import CourseAccessRole
+
+# Internal project dependencies
+from  utils import get_completed_percentage_per_user
+
+#TODO: obtain info
+def get_completion_per_user(info, course_key):
+    """
+    
+    """
+    staff_query = CourseAccessRole.objects.filter(
+                course_id=course_key
+            ).values('user_id')
+    enrolled_students = User.objects.filter(
+        courseenrollment__course_id=course_key,
+        courseenrollment__is_active=1
+    ).exclude(
+        id__in=staff_query
+    ).order_by('username').values('id', 'username', 'email')
+    return get_completed_percentage_per_user(info, enrolled_students, course_key)
