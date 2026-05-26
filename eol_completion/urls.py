db763333612cc1df603553ca7d23104c58fa05ec
Thomas Peet <tomaspemora@uchile.cl>
2026-05-26 14:50:01 -0400

    feat: add fast view and data endpoints to eol_completion urls

--- eol_completion/urls.py
+++ eol_completion/urls.py
@@ -2,8 +2,7 @@
 
 from django.conf.urls import url
 from django.conf import settings
-from .views import EolCompletionFragmentView, EolCompletionData
-
+from .views import EolCompletionFragmentView, EolCompletionData, EolCompletionFragmentFastView, EolCompletionDataFast
 
 urlpatterns = (
     url(
@@ -19,5 +18,19 @@ urlpatterns = (
         ),
         EolCompletionData.as_view(),
         name='completion_data_view',
+    ),
+    url(
+        r'courses/{}/student_completion_fast$'.format(
+            settings.COURSE_ID_PATTERN,
+        ),
+        EolCompletionFragmentFastView.as_view(),
+        name='completion_fast_view',
+    ),
+    url(
+        r'courses/{}/student_completion_fast/data$'.format(
+            settings.COURSE_ID_PATTERN,
+        ),
+        EolCompletionDataFast.as_view(),
+        name='completion_data_fast_view',
     ),
 )
