50359f9b9d9cbc54b3aa8c29f41cf3ba9a968c49
Thomas Peet <tomaspemora@uchile.cl>
2026-06-19 11:22:26 -0400

    fixup: delete new endpoints for fast implementation

--- eol_completion/urls.py
+++ eol_completion/urls.py
@@ -2,7 +2,7 @@
 
 from django.conf.urls import url
 from django.conf import settings
-from .views import EolCompletionFragmentView, EolCompletionData, EolCompletionFragmentFastView, EolCompletionDataFast
+from .views import EolCompletionFragmentView, EolCompletionData
 
 urlpatterns = (
     url(
@@ -18,19 +18,5 @@ urlpatterns = (
         ),
         EolCompletionData.as_view(),
         name='completion_data_view',
-    ),
-    url(
-        r'courses/{}/student_completion_fast$'.format(
-            settings.COURSE_ID_PATTERN,
-        ),
-        EolCompletionFragmentFastView.as_view(),
-        name='completion_fast_view',
-    ),
-    url(
-        r'courses/{}/student_completion_fast/data$'.format(
-            settings.COURSE_ID_PATTERN,
-        ),
-        EolCompletionDataFast.as_view(),
-        name='completion_data_fast_view',
     ),
 )
